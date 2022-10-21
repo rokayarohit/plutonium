c8de96a14dd89c22b7ed93716bb400dc2fc47b32
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2022-10-21 22:10:33 +0545

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,3 +1,9 @@
+// HTML Under Construction 😁
 <?php
 session_start();
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+if($user==admin && $pass==admin){
+echo "You're Welcome Mr. Plutonium";
+echo "<script>window.location.href='index.php';</script>";
 ?>
